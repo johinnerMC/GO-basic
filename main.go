7524b285e90362d411ff4299a3130202388b051c
johinner <johinner5721@gmail.com>
2022-01-31 14:45:01 -0500

    Primera clase ¿constantes y variables

--- main.go
+++ main.go
@@ -3,6 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	// Declaracion  de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.15
+	fmt.Println(pi, pi2)
 
-	fmt.Println("hola mundo")
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+
+	// Area dee un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuedrado = ", areaCuadrado)
+
 }
